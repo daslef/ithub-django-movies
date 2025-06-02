b4f6a1818bb5fe090038827a17aa3787e315bee7
Lex Trofimov <daslef93@gmail.com>
2025-06-02 17:02:43 +0300

    remove unused views

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import MoviesView, MovieView, handle_add_comment
+from .views import MoviesView
 
 urlpatterns = [
     path("", MoviesView.as_view(), name="movies"),
-    path("<int:pk>", MovieView.as_view(), name="movie"),
-    path("<int:pk>/add_comment", handle_add_comment, name="add_comment")
+    # path("<int:pk>", MovieView.as_view(), name="movie"),
+    # path("<int:pk>/add_comment", handle_add_comment, name="add_comment")
 ]
